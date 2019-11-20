553c7e44bd45bed8a30974c4d4106aa75f4e6a0e
liyiwen <915071628@qq.com>
2019-11-20 19:06:18 +0800

    0

---
+++ client/src/main/java/org/apache/rocketmq/client/impl/MQClientManager.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.client.impl;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.rocketmq.client.ClientConfig;
+import org.apache.rocketmq.client.impl.factory.MQClientInstance;
+import org.apache.rocketmq.client.log.ClientLogger;
+import org.apache.rocketmq.logging.InternalLogger;
+import org.apache.rocketmq.remoting.RPCHook;
+
+public class MQClientManager {
+    private final static InternalLogger log = ClientLogger.getLog();
+    private static MQClientManager instance = new MQClientManager();
+    private AtomicInteger factoryIndexGenerator = new AtomicInteger();
+    private ConcurrentMap<String/* clientId */, MQClientInstance> factoryTable =
+        new ConcurrentHashMap<String, MQClientInstance>();//用于管理mq客户端
+
+    private MQClientManager() {
+
+    }
+
+    public static MQClientManager getInstance() {
+        return instance;//很简单的一个单例模式
+    }
+
+    public MQClientInstance getAndCreateMQClientInstance(final ClientConfig clientConfig) {
+        return getAndCreateMQClientInstance(clientConfig, null);
+    }
+
+    public MQClientInstance getAndCreateMQClientInstance(final ClientConfig clientConfig, RPCHook rpcHook) {
+        String clientId = clientConfig.buildMQClientId();//基本的配置可以构成clientid ip + pid构成一个clientid 这台机器的这个进程所创建的所有生产者消费者都属于同一个客户端
+        MQClientInstance instance = this.factoryTable.get(clientId);
+        if (null == instance) {//如果不存在则构造MQClientInstance
+            instance =
+                new MQClientInstance(clientConfig.cloneClientConfig(),
+                    this.factoryIndexGenerator.getAndIncrement(), clientId, rpcHook);
+            MQClientInstance prev = this.factoryTable.putIfAbsent(clientId, instance);
+            if (prev != null) {
+                instance = prev;
+                log.warn("Returned Previous MQClientInstance for clientId:[{}]", clientId);
+            } else {
+                log.info("Created new MQClientInstance for clientId:[{}]", clientId);
+            }
+        }
+
+        return instance;
+    }
+
+    public void removeClientFactory(final String clientId) {
+        this.factoryTable.remove(clientId);
+    }
+}
